4b5d17def324b267ab22bb7d1c7a6cee365068a1
Carlos Ferreira <carlos@especializati.com.br>
2022-12-06 15:07:36 -0300

    feat: test autoload

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
-phpinfo();
+use Src\Example\Example;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$test = new Example;
+var_dump($test->say());
---
+++ src/Example/Example.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Src\Example;
+
+class Example
+{
+    public function say(): string
+    {
+        return 'hi';
+    }
+}
