0a8922d84b0cd9fdeea49a403efe2d8b13aabae5
Carlos Ferreira <carlos@especializati.com.br>
2022-12-08 15:23:09 -0300

    feat: expectations

--- src/Example/Math.php
+++ src/Example/Math.php
@@ -8,4 +8,21 @@ class Math
     {
         return $num1 + $num2;
     }
+
+    public function pi(): float
+    {
+        return 3.1415;
+    }
+
+    public function location(string $full = null): array|string
+    {
+        if ($full) {
+            return strtoupper($full);
+        }
+
+        return [
+            'latitude' => 999991231,
+            'longitude' => 413123,
+        ];
+    }
 }
--- tests/Example/MathTest.php
+++ tests/Example/MathTest.php
@@ -11,3 +11,26 @@ it('test sum', function () {
     $result = $math->sum(9000, 2);
     expect($result)->toBe(9002);
 });
+
+it('should return float', function () {
+    $math = new Math;
+    $result = $math->pi();
+
+    expect($result)->toBeFloat();
+});
+
+it('should return array or empty', function () {
+    $math = new Math;
+    $result = $math->location();
+
+    expect($result)->toBeArray();
+    expect($result)->toMatchArray([
+        'latitude' => 999991231,
+        'longitude' => 413123,
+    ]);
+
+    $result2 = $math->location('example');
+    expect($result2)->toBeString();
+    expect($result2)->toBe(strtoupper('example'));
+    // expect($result2)->not->toBeEmpty();
+});
